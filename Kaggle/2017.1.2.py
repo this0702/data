d8393dfaf692d5dabfdd004dea10a5987e8be746
this0702 <35016701+this0702@users.noreply.github.com>
2018-01-02 17:37:03 +0800

    add eda. and fe.

--- Kaggle/2017.1.2.py
+++ Kaggle/2017.1.2.py
@@ -15,4 +15,201 @@ from scipy.stats import norm, skew #for some statistics
 
 ###trick1
 pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x)) #Limiting floats output to 3 decimal points
+##读取数据
+train = pd.read_csv('../data_set/house_price/train.csv')
+test = pd.read_csv('../data_set/house_price/test.csv')
+
+#去不需要数据
+
+##下面是去除房子id
+
+###Save the 'Id' column
+train_ID = train['Id']
+test_ID = test['Id']
+
+###Now drop the  'Id' colum since it's unnecessary for  the prediction process.
+train.drop("Id", axis = 1, inplace = True)
+test.drop("Id", axis = 1, inplace = True)
+
+##下面是去除离群点，注意这些离群点过度去除会造成过拟合
+# fig, ax = plt.subplots()
+# ax.scatter(x = train['GrLivArea'], y = train['SalePrice'])
+# plt.ylabel('SalePrice', fontsize=13)
+# plt.xlabel('GrLivArea', fontsize=13)
+# plt.show()#画出图像，重点研究excel中联系紧密的点
+train = train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index)
+
+
+#trick2将目标变量norm，方便自己进行分析
+
+##qq图，折线拟合图，还有很多代码内trick
+# sns.distplot(train['SalePrice'] , fit=norm);
+#
+# # Get the fitted parameters used by the function
+# (mu, sigma) = norm.fit(train['SalePrice'])
+# print( '\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+#
+# #Now plot the distribution
+# plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)],
+#             loc='best')
+# plt.ylabel('Frequency')
+# plt.title('SalePrice distribution')
+#
+# #Get also the QQ-plot
+# fig = plt.figure()
+# res = stats.probplot(train['SalePrice'], plot=plt)
+# plt.show()
+
+##We use the numpy fuction log1p which  applies log(1+x) to all elements of the column
+train["SalePrice"] = np.log1p(train["SalePrice"])
+
+
+#EDA plus Feature engineering
+#trick3注意你的FE应该适用于所有集合上，并且将来做预测也要对那些属性做这些操作.
+ntrain = train.shape[0]
+ntest = test.shape[0]
+y_train = train.SalePrice.values
+
+all_data = pd.concat((train, test)).reset_index(drop=True)
+all_data.drop(['SalePrice'], axis=1, inplace=True)
+# print("all_data size is : {}".format(all_data.shape))
+
+
+##we should fill these missing data and and konw how to visulise missing data
+# all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+# all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+# missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
+# # missing_data.head(20)
+#
+#
+# f, ax = plt.subplots(figsize=(15, 12))
+# plt.xticks(rotation='90')
+# sns.barplot(x=all_data_na.index, y=all_data_na)
+# plt.xlabel('Features', fontsize=15)
+# plt.ylabel('Percent of missing values', fontsize=15)
+# plt.title('Percent missing data by feature', fontsize=15)
+
+#Correlation map to see how features are correlated with SalePrice
+corrmat = train.corr()
+plt.subplots(figsize=(12,9))
+sns.heatmap(corrmat, vmax=0.9, square=True)
+
+
+##imputing missing value ，trick4 中位数，众数(mode)或者none,或者相似数据.函数都有，去查
+
+all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
+all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
+all_data["Alley"] = all_data["Alley"].fillna("None")
+all_data["Fence"] = all_data["Fence"].fillna("None")
+all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
+#Group by neighborhood and fill in missing value by the median LotFrontage of all the neighborhood
+all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(
+    lambda x: x.fillna(x.median()))
+for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+    all_data[col] = all_data[col].fillna('None')
+
+for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+    all_data[col] = all_data[col].fillna(0)
+
+for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF','TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+    all_data[col] = all_data[col].fillna(0)
+
+for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+    all_data[col] = all_data[col].fillna('None')
+all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+all_data = all_data.drop(['Utilities'], axis=1)
+all_data["Functional"] = all_data["Functional"].fillna("Typ")
+all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
+all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
+all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
+all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
+all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
+all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
+
+
+#Check remaining missing values if any
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)
+missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
+missing_data.head()
+
+#是离散的就把变量类型改为str
+#MSSubClass=The building class
+all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
+#Changing OverallCond into a categorical variable
+all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+#Year and month sold are transformed into categorical features.
+all_data['YrSold'] = all_data['YrSold'].astype(str)
+all_data['MoSold'] = all_data['MoSold'].astype(str)
+
+
+#将下列列给离散的特征做成数字,方便分类
+from sklearn.preprocessing import LabelEncoder
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
+        'YrSold', 'MoSold')
+# process columns, apply LabelEncoder to categorical features
+for c in cols:
+    lbl = LabelEncoder()
+    lbl.fit(list(all_data[c].values))
+    all_data[c] = lbl.transform(list(all_data[c].values))
+
+# shape
+# print('Shape all_data: {}'.format(all_data.shape))
+
+##Since area related features are very important to determine house prices, we add one more feature which is the total area of basement, first and second floor areas of each house
+all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+
+
+##接下来把属性数据的峰度偏度用box-cox降低一下.
+
+skewness = skewness[abs(skewness) > 0.75]
+print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
+
+from scipy.special import boxcox1p
+
+skewed_features = skewness.index
+lam = 0.15
+for feat in skewed_features:
+    # all_data[feat] += 1
+    all_data[feat] = boxcox1p(all_data[feat], lam)
+
+# all_data[skewed_features] = np.log1p(all_data[skewed_features])
+
+##好了现在把这个问题二分类化
+all_data = pd.get_dummies(all_data)
+print(all_data.shape)
+
+##得到新的训练集和预测集
+train = all_data[:ntrain]
+test = all_data[ntrain:]
+
+#modelling
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import lightgbm as lgb
+
+##先定义五折交叉验证（trick5）
+#Validation function
+n_folds = 5
+def rmsle_cv(model):
+    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(train.values)
+    #这个rmse
+    rmse= np.sqrt(-cross_val_score(model, train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
+    return(rmse)
+
+#
+lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+
 
